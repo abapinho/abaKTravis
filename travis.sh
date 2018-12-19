cd1daab67d17cc3eda5ea56f4c9e9c50711d5c7f
Nuno Godinho <nununo@users.noreply.github.com>
2018-12-19 16:26:33 +0000

    Create travis.sh

---
+++ travis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+abapmerge src/zabak.prog.abap > ../zabak.abap
+wc -l ../zabak.abap
+cd ..
+git clone https://github.com/abapinho/abaK_build.git
+ls -l
+cp zabapgit.abap build/zabapgit.abap
+cd build
+git status
+git config --global user.email "builds@travis-ci.com"
+git config --global user.name "Travis CI"
+git add zabapgit.abap
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
+git push -q https://$GITHUB_API_KEY@github.com/abapGit/build.git > /dev/null 2>&1
